aea3e796b012a0cb81ac1110a4bc2e2020a71be9
Jackie1123 <1257868332@qq.com>
2019-08-31 17:12:34 +0800

    tenplate优化代码

--- pages/data/HomePageData.js
+++ pages/data/HomePageData.js
@@ -3,19 +3,11 @@ var initData = [{
     "authorIcon": "../images/me.png",
     "authorName": "小K同学",
     "authorDate": "2019/8/31",
-    "title": "你的图文标题",
-    "articleImg": "http://img.52z.com/upload/news/image/20180621/20180621055734_59936.jpg",
-    "articleText": "你的图文内容，你的图文内容，你的图文内容，你的图文内容，你的图文内容，你的图文内容，你的图文内容。"
+    "title": "习近平出席2019年国际篮联篮球世界杯开幕式",
+    "articleImg": "http://www.xinhuanet.com/politics/2019-08/30/1124943881_15671849263711n.jpg",
+    "articleText": "在现场观众倒计时的呼声中，开幕式开始。热情奔放的篮球少年上演活力四射的《我为篮球狂》。32个身着篮球服的少年儿童依次亮相，一一展现印有32个参赛国元素的篮球。"
   },
-  {
-    //"newsid": 1,
-    "authorIcon": "../images/me.png",
-    "authorName": "小K同学",
-    "authorDate": "2019/8/31",
-    "title": "你的图文标题",
-    "articleImg": "http://img.52z.com/upload/news/image/20180621/20180621055734_59936.jpg",
-    "articleText": "你的图文内容，你的图文内容，你的图文内容，你的图文内容，你的图文内容，你的图文内容，你的图文内容。"
-  },
+
   {
     //"newsid": 1,
     "authorIcon": "../images/me.png",
@@ -35,6 +27,17 @@ var initData = [{
     "articleImg": "http://img.52z.com/upload/news/image/20180621/20180621055734_59936.jpg",
     "articleText": "你的图文内容，你的图文内容，你的图文内容，你的图文内容，你的图文内容，你的图文内容，你的图文内容。"
   },
+
+  {
+    //"newsid": 1,
+    "authorIcon": "../images/me.png",
+    "authorName": "小K同学",
+    "authorDate": "2019/8/31",
+    "title": "你的图文标题",
+    "articleImg": "http://img.52z.com/upload/news/image/20180621/20180621055734_59936.jpg",
+    "articleText": "你的图文内容，你的图文内容，你的图文内容，你的图文内容，你的图文内容，你的图文内容，你的图文内容。"
+  },
+
   {
     //"newsid": 1,
     "authorIcon": "../images/me.png",
