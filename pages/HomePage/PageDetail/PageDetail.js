52821cc79128da61bb78fc7e9ae265bfbf6d8d02
Jackie1123 <1257868332@qq.com>
2019-09-01 14:32:35 +0800

    详情页的布局
    修复详情页每条新闻连在一起的bug

--- pages/HomePage/PageDetail/PageDetail.js
+++ pages/HomePage/PageDetail/PageDetail.js
@@ -1,10 +1,10 @@
-
+var newsData = require("../../data/HomePageData.js")
 Page({
   data: {
-
+    detailData: []
   },
 
-  onLoad: function (options) {
-    console.log(options.newsid)
+  onLoad: function(options) {
+    this.setData(newsData.initData[options.newsid])
   }
 })
--- pages/data/HomePageData.js
+++ pages/data/HomePageData.js
@@ -1,21 +1,23 @@
 var initData = [{
     "newsid": 0,
-    "authorIcon": "../images/me.png",
-    "authorName": "小K同学",
+  "authorIcon": "https://cn.bing.com/th?id=OIP.Un-kHTeFrNuZ7ZzYUdSx8gHaHa&pid=Api&rs=1",
+    "authorName": "新华网",
     "authorDate": "2019/8/31",
     "title": "习近平出席2019年国际篮联篮球世界杯开幕式",
     "articleImg": "http://www.xinhuanet.com/politics/2019-08/30/1124943881_15671849263711n.jpg",
-    "articleText": "在现场观众倒计时的呼声中，开幕式开始。热情奔放的篮球少年上演活力四射的《我为篮球狂》。32个身着篮球服的少年儿童依次亮相，一一展现印有32个参赛国元素的篮球。"
+    "articleMinText": "在现场观众倒计时的呼声中，开幕式开始。热情奔放的篮球少年上演活力四射的《我为篮球狂》。32个身着篮球服的少年儿童依次亮相，一一展现印有32个参赛国元素的篮球。",
+    "articleText": "在现场观众倒计时的呼声中，开幕式开始。热情奔放的篮球少年上演活力四射的《我为篮球狂》。32个身着篮球服的少年儿童依次亮相，一一展现印有32个参赛国元素的篮球。全场观众起立，奏唱中华人民共和国国歌，五星红旗冉冉升起。国际篮球联合会主席穆拉托在致辞中表示，2019年篮球世界杯是历史上办赛城市最多、规模最大、最受关注的一届赛事。他高度评价并衷心感谢中方精心的筹备工作。相信本届篮球世界杯一定会成功、精彩。应穆拉托的邀请，习近平主席宣布：2019年国际篮联篮球世界杯开幕！现场升起国际篮联会旗和2019年国际篮联篮球世界杯赛事旗帜。本届篮球世界杯吉祥物“梦之子”一声哨响，拉开“梦的畅想曲”文艺演出的帷幕。京剧与钢琴合鸣《天行健　人自强》。青年歌唱家齐唱《向梦想出发》。古琴演奏《众人力无不胜》，诠释共生共荣的东方哲学。叶沙篮球队唱响《壮志在我胸》，歌颂16岁的篮球少年叶沙去世后捐献出人体器官，受捐者组建篮球队为叶沙圆梦的故事。来自五大洲的篮球青年以及中国的艺术家们也献上了精彩的节目。中国篮球协会主席姚明护送本届世界杯冠军奖杯入场。代表8个比赛举办城市的篮球少年依次宣示：“中国准备好了!”孩子们唱起《加油！篮球》，表达对本届篮球世界杯的美好祝福。"
   },
 
   {
     "newsid": 1,
-    "authorIcon": "../images/me.png",
-    "authorName": "小K同学",
+    "authorIcon": "https://cn.bing.com/th?id=OIP.Un-kHTeFrNuZ7ZzYUdSx8gHaHa&pid=Api&rs=1",
+    "authorName": "XinHua News",
     "authorDate": "2019/8/31",
-    "title": "你的图文标题",
-    "articleImg": "http://img.52z.com/upload/news/image/20180621/20180621055734_59936.jpg",
-    "articleText": "你的图文内容，你的图文内容，你的图文内容，你的图文内容，你的图文内容，你的图文内容，你的图文内容。"
+    "title": "Hong Kong's business environment tarnished by protests",
+    "articleImg": "http://www.xinhuanet.com/english/2019-09/01/138354497_15672947980541n.jpg",
+    "articleMinText": "A passenger takes the Star Ferry from Hong Kong Island to Tsim Sha Tsui in Hong Kong, south China, Aug. 18, 2019.",
+    "articleText": "A passenger takes the Star Ferry from Hong Kong Island to Tsim Sha Tsui in Hong Kong, south China, Aug. 18, 2019. Hong Kong's favorable international image and business environment has been tarnished as violent and disruptive protests have been going on for more than two months. The disturbance's direct and negative effects on the economy are visible in the retail, catering and tourism industries, which have to resort to layoffs amid the sharp declines in sales, customer numbers and revenue. In the June-July period, average earnings for tourism workers plunged by 74 percent, while the number of tours in Hong Kong fell 74 percent on average, according to a survey by the Hong Kong Tour Guides General Union.Tourists are seen at the Avenue of Stars in Tsim Sha Tsui of Hong Kong, south China, Aug. 20, 2019. Hong Kong's favorable international image and business environment has been tarnished as violent and disruptive protests have been going on for more than two months. The disturbance's direct and negative effects on the economy are visible in the retail, catering and tourism industries, which have to resort to layoffs amid the sharp declines in sales, customer numbers and revenue."
   },
 
   {
@@ -25,7 +27,8 @@ var initData = [{
     "authorDate": "2019/8/31",
     "title": "你的图文标题",
     "articleImg": "http://img.52z.com/upload/news/image/20180621/20180621055734_59936.jpg",
-    "articleText": "你的图文内容，你的图文内容，你的图文内容，你的图文内容，你的图文内容，你的图文内容，你的图文内容。"
+    "articleMinText": "你的图文内容，你的图文内容，你的图文内容，你的图文内容，你的图文内容，你的图文内容，你的图文内容。",
+    "articleText": ""
   },
 
   {
@@ -35,7 +38,8 @@ var initData = [{
     "authorDate": "2019/8/31",
     "title": "你的图文标题",
     "articleImg": "http://img.52z.com/upload/news/image/20180621/20180621055734_59936.jpg",
-    "articleText": "你的图文内容，你的图文内容，你的图文内容，你的图文内容，你的图文内容，你的图文内容，你的图文内容。"
+    "articleMinText": "你的图文内容，你的图文内容，你的图文内容，你的图文内容，你的图文内容，你的图文内容，你的图文内容。",
+    "articleText": ""
   },
 
   {
@@ -45,7 +49,8 @@ var initData = [{
     "authorDate": "2019/8/31",
     "title": "你的图文标题",
     "articleImg": "http://img.52z.com/upload/news/image/20180621/20180621055734_59936.jpg",
-    "articleText": "你的图文内容，你的图文内容，你的图文内容，你的图文内容，你的图文内容，你的图文内容，你的图文内容。"
+    "articleMinText": "你的图文内容，你的图文内容，你的图文内容，你的图文内容，你的图文内容，你的图文内容，你的图文内容。",
+    "articleText": ""
   }
 ]
 
